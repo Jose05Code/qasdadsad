34de13b1364def4e4bc7e81a8abdf56121e987be
Jose Frade Gala <josefradegala@gmail.com>
2024-05-28 04:06:54 -0400

    a

--- testa/testa/settings.py
+++ testa/testa/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-px1oh00w@k-c+j($k+cpsgxy!kn%!5np%jk!kn^brm#349fkn&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*','https://lcd8s3bs-5173.use.devtunnels.ms/login']
 
@@ -134,3 +134,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://lcd8s3bs-5173.use.devtunnels.ms",
     "http://localhost:5173"
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
