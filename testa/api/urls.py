c4fe75e9b16f592827bba9213845fecacc9d273f
Jose Frade Gala <josefradegala@gmail.com>
2024-06-03 19:09:13 -0400

    new endpoint items

--- testa/api/urls.py
+++ testa/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 from .views import send_picture
+from .views import ItemList
 
 urlpatterns = [
     path('pictures/<str:name>/', send_picture, name='send_picture'),
+    path('items/', ItemList, name='item_list')
     # Add more paths here
 ]
--- testa/api/views.py
+++ testa/api/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404
 from django.http import FileResponse
-from .models import Item
+from .models import Item, SalesItem, InventoryItem  # Import the missing models
 from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
 @csrf_exempt
 def send_picture(request, name):
@@ -16,3 +17,29 @@ def send_picture(request, name):
 
     # Devuelve la imagen como una respuesta HTTP
     return FileResponse(img)
+
+@csrf_exempt
+def ItemList(request):
+    # Obtén todos los items
+    items = Item.objects.all()
+
+    # Crea una lista para almacenar los datos de cada item
+    item_data = []
+    
+    # Itera sobre cada item y obtén su información
+    for item in items:
+        # Obtén el objeto SalesItem correspondiente
+        sales_item = SalesItem.objects.get(item=item)
+        # Obtén el objeto InventoryItem correspondiente
+        inventory_item = InventoryItem.objects.get(item=item)
+
+        item_info = {
+            'name': item.name,
+            'price': sales_item.price,
+            'picture': item.picture.url,
+            'quantity': inventory_item.quantity
+        }
+        item_data.append(item_info)
+    
+    # Devuelve los items como una respuesta HTTP en formato JSON
+    return JsonResponse({'items': item_data})
