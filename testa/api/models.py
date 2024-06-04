ec8795fdfccffb9b4d040d82e488df4b74f67662
Jose Frade Gala <josefradegala@gmail.com>
2024-06-03 22:06:06 -0400

    try and except for item list

--- testa/api/models.py
+++ testa/api/models.py
@@ -1,10 +1,39 @@
 from django.db import models
+import boto3
+from django.conf import settings
+
+def get_upload_to(instance, filename):
+    # Obtiene la extensión del archivo
+    extension = filename.split('.')[-1]
+
+    # Crea el nuevo nombre del archivo
+    new_filename = '{}.{}'.format(instance.name, extension)
+
+    # Retorna la ruta completa del archivo
+    return 'item_pictures/{}'.format(new_filename)
 
 class Item(models.Model):
     name = models.CharField(max_length=100)
     sku = models.IntegerField(unique=True)
     gtin = models.IntegerField(unique=True)
-    picture = models.ImageField(upload_to='item_pictures/', blank=True)
+    picture = models.ImageField(upload_to=get_upload_to, blank=True)
+
+
+    def delete(self, *args, **kwargs):
+        # Crea un cliente de S3
+        s3 = boto3.client('s3')
+
+        # Obtiene el nombre del archivo de la imagen
+        picture_name = self.picture.name
+
+        # Elimina el archivo de S3
+        s3.delete_object(
+            Bucket=settings.AWS_STORAGE_BUCKET_NAME,
+            Key=picture_name,
+        )
+
+        # Llama al método delete original
+        super().delete(*args, **kwargs)
 
     def __str__(self):
         return self.name
--- testa/api/views.py
+++ testa/api/views.py
@@ -29,18 +29,21 @@ def ItemList(request):
     
     # Itera sobre cada item y obtén su información
     for item in items:
-        # Obtén el objeto SalesItem correspondiente
-        sales_item = SalesItem.objects.get(item=item)
-        # Obtén el objeto InventoryItem correspondiente
-        inventory_item = InventoryItem.objects.get(item=item)
+        try:
+            # Obtén el objeto SalesItem correspondiente
+            sales_item = SalesItem.objects.get(item=item)
+            # Obtén el objeto InventoryItem correspondiente
+            inventory_item = InventoryItem.objects.get(item=item)
 
-        item_info = {
-            'name': item.name,
-            'price': sales_item.price,
-            'picture': f'https://django-alpha-eosin.vercel.app/item_picture/{item.name}/',
-            'quantity': inventory_item.quantity,
-        }
-        item_data.append(item_info)
+            item_info = {
+                'name': item.name,
+                'price': sales_item.price,
+                'picture': f'https://django-alpha-eosin.vercel.app/item_picture/{item.name}/',
+                'quantity': inventory_item.quantity,
+            }
+            item_data.append(item_info)
+        except (SalesItem.DoesNotExist, InventoryItem.DoesNotExist):
+            continue
     
     # Devuelve los items como una respuesta HTTP en formato JSON
     return JsonResponse({'items': item_data})
